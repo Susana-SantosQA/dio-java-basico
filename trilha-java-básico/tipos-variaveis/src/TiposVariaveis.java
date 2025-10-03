fc0086151dfd18996256fd8c6e9ad2517361cacc
Susana dos Santos <criaoesfantasticas@gmail.com>
2025-10-03 20:37:43 -0300

    primeiro commit

---
+++ trilha-java-básico/tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,17 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        // Tipos primitivos
+        //estudem a classe String que representa textos na aplicação
+
+
+        System meuNome = "Susana Santos";
+
+        dounble salarioMinimo = 2500.00;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+    }
+}
