48ed66016c94739aa3db3fb51fce552ecdca4223
Susana dos Santos <criaoesfantasticas@gmail.com>
2025-10-04 22:46:14 -0300

    primeiro commit

---
+++ trilha-java-básico/java-terminal/src/Calculadora.java
@@ -0,0 +1,23 @@
+/**
+ * <h1>Calculadora</h1>
+ * A calculadora realiza operações matemáticas entre numeros inteiros.
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação desta classes
+ * para desfrutar dos recursos oferecidos pelo autor.
+ * 
+ * @autor Susana Santos
+ * @version 1.0
+ * @since 04/10/2025
+ */
+public class Calculadora {
+    /**
+     * Este metodo é utilizado para somar dois numeros inteiros.
+     * @param numeroUm este é o primeiro parametro do método
+     * @param numeroDois este é o segundo parametro do método
+     * @return int o resultado desde método é a soma dos dois numeros
+     * @throws IllegalArgumentException Se a operação não for "S" ou "M"
+     */
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+}
---
+++ trilha-java-básico/java-terminal/src/Comentarios.java
@@ -0,0 +1,23 @@
+public class Comentarios {
+    
+    public static void main(String[] args) {
+        
+    }
+    /*
+     * Este metodo foi elaborado as pressas
+     * por isso eu abreviei o nome das variaveis
+     * mas olha, ele tem a finalidade somar ou multiplicar
+     * dois numeros
+     */
+
+    public int somaMultiplica (int n, int x, String m){
+    int r = 0; //r é igual ao resultado
+    if (m == "M"){ // M é multiplicação
+        r = n * x;
+    } else{
+        // S é soma
+        r = n * x;
+    }
+    return r;
+    }
+}
